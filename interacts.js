89e83c005e40ccc4c755990ee418c4a279734d4f
cyber-chalk <luka.chalker@gmail.com>
2023-02-26 23:24:50 +1100

    almost done

--- interacts.js
+++ interacts.js
@@ -1,16 +1,13 @@
 let keys = document.getElementsByTagName("kbd");
 document.addEventListener("keydown", (event) => {
-	// console.log(event, event.key.toUpperCase());
 	for (let i = 0; i < keys.length; i++) {
-		console.log(keys[i], event.key.toUpperCase());
 		if (keys[i].innerHTML == event.key.toUpperCase()) {
-			console.log(keys[i], event.key.toUpperCase());
 			keys[i].style =
 				"background-color: #303236; border: solid 3px #44474c; border-bottom-color: #44474c; border-radius: 0.933vw; box-shadow: inset 0 -3px 0 #303236;";
 		}
 	}
 });
- 
+
 document.addEventListener("keyup", (event) => {
 	for (let i = 0; i < keys.length; i++) {
 		if (event.key.toUpperCase() == keys[i].innerHTML) keys[i].style = "";
@@ -23,3 +20,13 @@ function button(element) {
 		element.style.backgroundColor = "";
 	}, 100);
 }
+
+let options = document.getElementsByClassName("options");
+function switcher(element) {
+	element.style.backgroundColor = "rgba(43, 43, 43, 0.73)";
+
+	options[element.dataset.other].style.backgroundColor = "";
+}
+
+// by default
+options[1].style.backgroundColor = "rgba(43, 43, 43, 0.73)";
