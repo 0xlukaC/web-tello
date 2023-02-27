45e8d40c7c98ca498247651b351cae225bcec01a
cyber-chalk <luka.chalker@gmail.com>
2023-02-27 22:04:43 +1100

    interactivity

--- interacts.js
+++ interacts.js
@@ -22,11 +22,40 @@ function button(element) {
 }
 
 let options = document.getElementsByClassName("options");
+let currentType = options[1];
+
 function switcher(element) {
 	element.style.backgroundColor = "rgba(43, 43, 43, 0.73)";
 
 	options[element.dataset.other].style.backgroundColor = "";
+	console.log(element, options);
+	currentType = element;
+	resetOpacity();
 }
 
 // by default
 options[1].style.backgroundColor = "rgba(43, 43, 43, 0.73)";
+
+//option[1] is photo
+//option[0] is video
+
+function capture(element) {
+	let inner = element.querySelectorAll("circle")[1];
+	if (currentType == options[1]) {
+		inner.style.opacity = "0.7";
+		setTimeout(() => {
+			inner.style.opacity = "1";
+		}, 100);
+		return;
+	}
+	if (currentType == options[0] && inner.style.opacity == 0.7)
+		return (inner.style.opacity = "1");
+	//else
+	return (inner.style.opacity = "0.7");
+}
+
+function resetOpacity() {
+	let element = document.getElementById("record");
+	let inner = element.querySelectorAll("circle")[1];
+	inner.style.opacity = "1";
+}
