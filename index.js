fb86f297c1cb4f0d190491b6368622bafbf0f750
cyber-chalk <luka.chalker@gmail.com>
2023-03-19 15:27:38 +1100

    ah

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ app.get("/", (req, res) => {
 
 app.listen(webport);
 
+//start of tello connection
+
 const dgram = require("dgram");
 const host = "192.168.10.1";
-const port = "8889";
 
-//creates the client UDP
+// Send Command & Receive Response
+const port = "8889";
 const client = dgram.createSocket("udp4");
 
 client.on("message", (msg, rinfo) => {
@@ -31,9 +33,52 @@ client.on("error", (err) => {
 
 client.on("listening", () => {
 	let clientAddress = client.address();
-	console.log(`listening on ${clientAddress.address}:${clientAddress.port}`);
+	console.log(
+		`listening for command on ${clientAddress.address}:${clientAddress.port}`
+	);
 });
 client.bind(port);
+
+// Receive Tello Video Stream
+
+const video = dgram.createSocket("udp4");
+const videoPort = 1111; //may have to change to number
+const videoServer = "0.0.0.0";
+
+video.on("listening", () => {
+	let clientAddress = video.address();
+	console.log(`VIDEO is A OK ${clientAddress.address}:${clientAddress.port}`);
+});
+
+video.on("message", (msg, rinfo) => {
+	// console.log(`Video message: ${msg}`);
+	console.log("hello");
+	console.log(msg);
+});
+
+video.on("error", (err) => {
+	console.log("there is a error", err);
+});
+
+// video.bind(videoServer, videoPort);
+video.bind({
+	port: videoPort,
+	address: videoServer
+});
+// Recieve state
+
+// function parseState(state) {
+// 	return state
+// 		.split(";")
+// 		.map((x) => x.split(":"))
+// 		.reduce((data, [key, value]) => {
+// 			data[key] = value;
+// 			return data;
+// 		}, {});
+// }
+
+// const droneState = dgram.createSocket("udp4");
+// droneState.bind(8890);
 
 //creates the command line interface
 
@@ -53,6 +98,7 @@ rl.on("line", (line) => {
 
 //sends the message
 function messanger(msg) {
+	console.log("ok", msg);
 	const buffer = Buffer.from(msg);
 	client.send(buffer, 0, buffer.length, port, host, (err, bytes) => {
 		if (err) {
@@ -60,7 +106,8 @@ function messanger(msg) {
 		}
 	});
 }
-messanger("command");
+// messanger("command");
+// messanger("streamon");
 
 //closes the CLI and stops the program
 function closeApp() {
